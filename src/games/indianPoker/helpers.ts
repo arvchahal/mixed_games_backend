e0bd41098027259592320015ecf3bf1200f5c8bc
arvchahal <arnavchahal14@gmail.com>
2026-02-28 01:52:23 -0600

    small tweak

--- src/games/indianPoker/helpers.ts
+++ src/games/indianPoker/helpers.ts
@@ -1,8 +1,7 @@
 import { IndianPokerPlayer, HandState } from "./types";
 export function getCurrentPlayer(hand: HandState): IndianPokerPlayer {
     let curr_idx: number = hand.currentPlayerIndex;
-    let adjusted_idx: number = curr_idx % Object.keys(hand.players).length;
-    let player_key: string = hand.playerOrder[adjusted_idx];
+    let player_key: string = hand.playerOrder[curr_idx];
     let curr_player: IndianPokerPlayer = hand.players[player_key];
     return curr_player;
 }
